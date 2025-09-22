f4af52dacdda7bdcce1a25ac52c1ff342d26a69c
Hoesu <hoesu.chung@qraftec.com>
2025-09-22 19:08:25 +0900

    FEAT: simple execution script

--- assignment_01/main.py
+++ assignment_01/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from assignment-01!")
+from src.human import HumanImageCompressor
+from src.ai import AiImageCompressor
 
+def main():
+    human_image_compressor = HumanImageCompressor("assets/silksong.jpeg")
+    human_image_compressor._reconstruct_image()
+    
+    ai_image_compressor = AiImageCompressor("assets/silksong.jpeg")
+    ai_image_compressor._reconstruct_image()
 
 if __name__ == "__main__":
     main()
